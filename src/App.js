de6b5385cd7fd645c8890c004c8c7da65e65b6de
Maxwell Dubin <maxdhs@gmail.com>
2020-06-04 13:27:18 -0400

    Week 3 Pre-Assignment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Directory from "./components/DirectoryComponent";
-import "./App.css";
-import { CAMPSITES } from "./shared/campsites";
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
-        <Directory campsites={this.state.campsites} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
